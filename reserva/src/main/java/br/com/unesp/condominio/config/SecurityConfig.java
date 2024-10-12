0ab061a171dd9b5a6dac6ab4d500f23d40d67715
tmferreira-ti <163651394+tmferreira-ti@users.noreply.github.com>
2024-10-11 22:17:40 -0300

    Ldap Ok

--- reserva/src/main/java/br/com/unesp/condominio/config/SecurityConfig.java
+++ reserva/src/main/java/br/com/unesp/condominio/config/SecurityConfig.java
@@ -3,21 +3,29 @@ package br.com.unesp.condominio.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.ldap.authentication.ad.ActiveDirectoryLdapAuthenticationProvider;
 import org.springframework.security.web.SecurityFilterChain;
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests(authorize -> authorize
-                        .anyRequest().permitAll() // Permitir todas as requisições sem autenticação
+                .authorizeRequests(authorizeRequests ->
+                        authorizeRequests.anyRequest().authenticated()
                 )
-                .csrf().disable(); // Desabilitar CSRF para testes mais fáceis com POST
-
+                .formLogin(withDefaults());
         return http.build();
+    }
+
+    @Bean
+    public ActiveDirectoryLdapAuthenticationProvider activeDirectoryLdapAuthenticationProvider() {
+        ActiveDirectoryLdapAuthenticationProvider provider =
+                new ActiveDirectoryLdapAuthenticationProvider("unesp.local", "ldaps://unesp.local:636/");
+        provider.setConvertSubErrorCodesToExceptions(true);
+        provider.setUseAuthenticationRequestCredentials(true);
+        return provider;
     }
 }
